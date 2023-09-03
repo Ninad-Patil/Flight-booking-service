ef946727273bcf81c19dbdeb3fcfdd0e993baf9d
ninad patil <ninad.patil198@gmail.com>
2023-09-03 22:52:17 +0530

    made proxy compliant

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
+app.use("/bookingService/api", apiRoutes);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`server running on port ${serverConfig.PORT}`);
